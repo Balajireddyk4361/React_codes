99e87f25503f15a50b92226769194188858e8a58
K Balajireddy <balajireddyk4451@gmail.com>
2025-04-25 16:46:02 +0530

    react installer

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+
+// createRoot(document.getElementById('BalajiReddy')).render(<div><h>Hello Good Afternoon</h><br></br>
+// <h>Hello Good Afternoon</h><br></br>
+// <h>Hello Good Afternoon</h>
+// </div>)
+
+import App from './App'
+
+createRoot(document.getElementById('root')).render(<App/>)
